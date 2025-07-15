9ddcf0a6807cd2b945dc70558fc0e05526323b24
Robert73 <robertnjunji@gmail.com>
2025-07-15 20:46:39 +0300

    Initial commit

---
+++ fruit.js
@@ -0,0 +1,7 @@
+function checkFruit(){
+  const input = document.getElementById("fruit").value.toLowerCase();
+  const fruits = ["mango", "orange", "apple", "banana"];
+  const found = fruits.includes(input);
+  
+  document.getElementById("output").innerText = found? `✅${input} is available` :`❌${input} is not in the list` ;
+}
